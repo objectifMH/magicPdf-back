056b5bb27e44433bf94acc76050ba4a3c3a34056
objectifmh <objectifmh@hotmail.com>
2019-09-11 16:58:33 +0100

    upload + split

--- src/main/java/fr/epsi/manager/PdfResource.java
+++ src/main/java/fr/epsi/manager/PdfResource.java
@@ -14,6 +14,7 @@ import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.*;
 import static com.itextpdf.text.pdf.PdfName.URL;
+import com.sun.jersey.multipart.FormDataParam;
 
 import java.util.List;
 import javax.ws.rs.core.Context;
@@ -52,6 +53,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response.Status;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+
 /**
  * REST Web Service
  *
@@ -61,13 +63,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("*")
 public class PdfResource {
 
+    static int inc =  1 ; 
+    
     @Context
     private UriInfo context;
 
     private String DATA = "C:\\pdf\\test1.pdf";
 
     private String DEST = "C:\\pdf\\Dest\\";
-    private String CHEMIN = "C:\\pdf\\";
+    private String CHEMIN = "C:\\pdf\\Dest\\split\\";
+    private String CHEMIN_MERGE = "C:\\pdf\\Dest\\";
 
     private String TEST_PDF = "C:\\pdf\\HelloWorld.pdf";
     private String MERGE_PDF = "C:\\pdf\\merge.pdf";
@@ -84,8 +89,11 @@ public class PdfResource {
      * Retrieves representation of an instance of
      * fr.epsi.manager.GenericResource
      *
+     * @param pdfFile
+     * @param path
      * @param uploadedInputStream
      * @return an instance of java.lang.String
+     * @throws java.io.IOException
      */
     /*
     @GET
@@ -101,28 +109,29 @@ public class PdfResource {
 }*/
     @POST
     @Path("/upload")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile( File pdfFile ) throws IOException {
-        String nomPdf = "test1.pdf";
-        System.out.println("Je suis dans l'upload" + pdfFile.getName());
+    @Consumes(MediaType.MULTIPART_FORM_DATA)  
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadFile(  @FormDataParam("fileUp") File pdfFile    ) throws IOException {
         
-        String res = "pas de fichier" ; 
+        inc++;
+        String nomPdf = "upload"+inc+".pdf";
+        // @FormParam("fileUp") File fileUpload , @FormParam("fileUp") String nomFile 
+        
         if ( pdfFile.length() > 0 )
         {
-            
-            System.out.println("le chemin de mon fichier "+ pdfFile.getName());
-            copyFiles(pdfFile, pdfFile.getName());
+            System.out.println("Nom temp "+ pdfFile.getName());
+            copyFiles(pdfFile, nomPdf);
         }
-        
+        System.out.println("Nom sur le serveur : "+ nomPdf);
         return Response
                 .status(Status.OK)
+                .entity("{\"nomPdf\":\""+nomPdf+"\"}")
                 .build();
     }
 
     public  void copyFiles(File sourceLocation, String nomPdf )
     throws IOException {
 
-        
         {
            
                 InputStream in = new FileInputStream(sourceLocation);
@@ -251,24 +260,27 @@ public class PdfResource {
 
     @GET // M�thode HTTP utilis�e pour d�clencher cette m�thode
     @Path("/split")
-    public String split() throws IOException, DocumentException {
-        System.out.println("split  ");
-        String monSt = "split de Pdf ";
+    public Response split( @QueryParam("pdfSplit") String pdfSplit) throws IOException, DocumentException {
+        System.out.println("Je suis dans le split  ");
+        
 
         // read original pdf file
-        String filename = "pdf1.pdf";
-        PdfReader reader = new PdfReader(CHEMIN + filename);
+        String filename = pdfSplit; //"pdf1.pdf";
+        System.out.println(" le chemin {" + CHEMIN_MERGE + pdfSplit +"} [" + pdfSplit + "]");
+        //PdfReader reader = new PdfReader(CHEMIN + "merge1.pdf");
+        PdfReader reader = new PdfReader(CHEMIN_MERGE + filename); //CHEMIN = "C:\\pdf\\Dest\\";
 
         // get number of pages
-        int n = reader.getNumberOfPages();
-        System.out.println("Number of pages: " + n);
+                int n = reader.getNumberOfPages();
+                System.out.println("Number of pages: " + n);
 
         // loop over all pages
+         /*
         int i = 0;
         while (i < n) {
 
             // create destination file name
-            String destination = CHEMIN + filename.substring(0, filename.indexOf(".pdf")) + "-" + String.format("%03d", i + 1) + ".pdf";
+            String destination = CHEMIN_MERGE + filename.substring(0, filename.indexOf(".pdf")) + "-" + String.format("%03d", i + 1) + ".pdf";
             System.out.println("Writing " + destination);
 
             // create new document with corresponding page size
@@ -285,8 +297,10 @@ public class PdfResource {
             // close and write the document
             document.close();
         }
-
-        return monSt;
+         */
+        return Response
+                .status(Status.OK)
+                .build();
 
     }
 
